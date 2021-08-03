836bd23a0019ecd9ea1e55959ba6ae706ad74b6b
Halle Whittaker <whittaker.halle@gmail.com>
2021-08-03 17:49:54 +0100

    Halle's current progress

---
+++ Git2CSV.py
@@ -0,0 +1,35 @@
+import subprocess
+r= subprocess.run('git --no-pager blame --line-porcelain README.md', stdout= subprocess.PIPE)
+#currently uses system command line to read a specific document.
+
+tab = r.stdout.split(b'\t') 
+for lineinfoS in tab:
+    #print(lineinfoS)
+    print("----")
+    linearray= lineinfoS.split(b'\n')
+    for individL in linearray:
+        print(individL)
+        print("____")
+#currently splits tabs and new lines and prints this edited format.
+
+cheader =[]
+cdata = []
+
+import re
+a="b'filename README.md'" #work
+b="b'boundary'" #work
+c="b'author-mail <88335095+hallewhittaker@users.noreply.github.com>'" #work
+
+pattern = r"\s+|[']|'(?=\s)"
+result = [s for s in re.split(pattern, c) if s]
+print(result)
+
+cheader.append(result[1])
+print(cheader)
+
+if len(result) >2:
+    cdata.append(result[2])       
+else:
+    cdata.append('N/A')
+print(cdata) 
+#currently uses a string, splits said string into column header + data
