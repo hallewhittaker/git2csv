a06a614cb5cd78ca4e5015f440de347f4e24870d
Halle Whittaker <whittaker.halle@gmail.com>
2021-08-03 20:24:20 +0100

    Halle Updated with splitlines #4

--- Git2CSV.py
+++ Git2CSV.py
@@ -1,51 +1,51 @@
 import subprocess
-# testdictionary1= {"email": "whittaker@gmail.com", "message": "hello welcom"}
-# testdictionary2= {"email": "like@gmail.com", "message": "bye welcom"}
-
-# #print(testdictionary1["email"])
-# arrayofD= [testdictionary1,testdictionary2]
-
-# #print(arrayofD)
-
-# for currentDictionary in arrayofD:
-#     print(currentDictionary["email"])
-
-
-
 # r= subprocess.run('git --no-pager blame --line-porcelain README.md',s stdout= subprocess.PIPE)
 #currently uses system command line to read a specific document.
+
 # tab = r.stdout.split(b'\t') 
 # print(r.stdout)
 
 test_data = b"author hallewhittaker\nauthor-mail <88335095+hallewhittaker@users.noreply.github.com>\nauthor-time 1627917128\nauthor-tz +0100\ncommitter GitHub\ncommitter-mail <noreply@github.com>\ncommitter-time 1627917128\ncommitter-tz +0100\nsummary Create README.md\nboundary\nfilename README.md\n\t--->\n'"
 tab = test_data.split(b'\t') 
 
-
 arrayofDictionaries = []
-
+# current string = b'author': b'author hallewhittaker' .split(b" ")[0]
+# new string = b'author': hallewhittaker'
+#b'author hallewhittaker': b'author hallewhittaker' without .split
 
 for lineinfoS in tab:
-    print(lineinfoS)
-    print("----")
+    #print(lineinfoS)
+    #print("----")
     linearray= lineinfoS.split(b'\n')
     tempdictionary = {}
+
     for individL in linearray:
-        print(individL)
-        tempkeyN = individL.split(b" ")[0]
-        print(tempkeyN)
-
-        print("____")
-        tempdictionary[tempkeyN] = individL
-        # breaks
+        #print(individL)
+        splitline = individL.split(b" ")
+        tempkeyN = splitline[0]
+        #print(splitline) 
+        splitline[0] = b" "
+        
+        joinline = b" ".join(splitline)
+        #print(joinline)
+        tempdictionary[tempkeyN] = joinline
     arrayofDictionaries.append(tempdictionary)
-    # break
-
-# this just prints things out nicely
-# import json
 print(arrayofDictionaries)
 
-#currently splits tabs and new lines and prints this edited format.
 
+
+
+
+
+
+
+#print(tempkeyN)
+
+        #print("____")
+
+
+
+#Using re.split
 # cheader =[]
 # cdata = []
 
@@ -67,3 +67,15 @@ print(arrayofDictionaries)
 #     cdata.append('N/A')
 # print(cdata) 
 #currently uses a string, splits said string into column header + data
+
+#Using Dictionaries
+# testdictionary1= {"email": "whittaker@gmail.com", "message": "hello welcom"}
+# testdictionary2= {"email": "like@gmail.com", "message": "bye welcom"}
+
+# #print(testdictionary1["email"])
+# arrayofD= [testdictionary1,testdictionary2]
+
+# #print(arrayofD)
+
+# for currentDictionary in arrayofD:
+#     print(currentDictionary["email"])
