e07c302c749fd090bbb4528eb5b098d1d86c9a1e
Halle Whittaker <whittaker.halle@gmail.com>
2021-08-03 17:57:39 +0100

    Using dictionaries

--- Git2CSV.py
+++ Git2CSV.py
@@ -1,35 +1,43 @@
 import subprocess
-r= subprocess.run('git --no-pager blame --line-porcelain README.md', stdout= subprocess.PIPE)
+test =["whittaler@gmail.com", "hello welcom"]
+testdictionary= {"email": "whittaker@gmail.com", "message": "hello welcom"}
+
+print(test)
+print(test[0])
+
+print(testdictionary["email"])
+
+# r= subprocess.run('git --no-pager blame --line-porcelain README.md', stdout= subprocess.PIPE)
 #currently uses system command line to read a specific document.
 
-tab = r.stdout.split(b'\t') 
-for lineinfoS in tab:
-    #print(lineinfoS)
-    print("----")
-    linearray= lineinfoS.split(b'\n')
-    for individL in linearray:
-        print(individL)
-        print("____")
+# tab = r.stdout.split(b'\t') 
+# for lineinfoS in tab:
+#     #print(lineinfoS)
+#     print("----")
+#     linearray= lineinfoS.split(b'\n')
+#     for individL in linearray:
+#         print(individL)
+#         print("____")
 #currently splits tabs and new lines and prints this edited format.
 
-cheader =[]
-cdata = []
+# cheader =[]
+# cdata = []
 
-import re
-a="b'filename README.md'" #work
-b="b'boundary'" #work
-c="b'author-mail <88335095+hallewhittaker@users.noreply.github.com>'" #work
+# import re
+# a="b'filename README.md'" #work
+# b="b'boundary'" #work
+# c="b'author-mail <88335095+hallewhittaker@users.noreply.github.com>'" #work
 
-pattern = r"\s+|[']|'(?=\s)"
-result = [s for s in re.split(pattern, c) if s]
-print(result)
+# pattern = r"\s+|[']|'(?=\s)"
+# result = [s for s in re.split(pattern, c) if s]
+# print(result)
 
-cheader.append(result[1])
-print(cheader)
+# cheader.append(result[1])
+# print(cheader)
 
-if len(result) >2:
-    cdata.append(result[2])       
-else:
-    cdata.append('N/A')
-print(cdata) 
+# if len(result) >2:
+#     cdata.append(result[2])       
+# else:
+#     cdata.append('N/A')
+# print(cdata) 
 #currently uses a string, splits said string into column header + data
