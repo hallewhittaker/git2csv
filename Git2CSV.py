751b00ca02983d1277963713c154cb7cf19c102e
Halle Whittaker <whittaker.halle@gmail.com>
2021-08-03 18:31:37 +0100

    Halle updated code #3

--- Git2CSV.py
+++ Git2CSV.py
@@ -1,28 +1,49 @@
 import subprocess
-testdictionary1= {"email": "whittaker@gmail.com", "message": "hello welcom"}
-testdictionary2= {"email": "like@gmail.com", "message": "bye welcom"}
+# testdictionary1= {"email": "whittaker@gmail.com", "message": "hello welcom"}
+# testdictionary2= {"email": "like@gmail.com", "message": "bye welcom"}
 
-#print(testdictionary1["email"])
-arrayofD= [testdictionary1,testdictionary2]
+# #print(testdictionary1["email"])
+# arrayofD= [testdictionary1,testdictionary2]
 
-#print(arrayofD)
+# #print(arrayofD)
 
-for currentDictionary in arrayofD:
-    print(currentDictionary["email"])
+# for currentDictionary in arrayofD:
+#     print(currentDictionary["email"])
 
 
 
-# r= subprocess.run('git --no-pager blame --line-porcelain README.md', stdout= subprocess.PIPE)
+# r= subprocess.run('git --no-pager blame --line-porcelain README.md',s stdout= subprocess.PIPE)
 #currently uses system command line to read a specific document.
-
 # tab = r.stdout.split(b'\t') 
-# for lineinfoS in tab:
-#     #print(lineinfoS)
-#     print("----")
-#     linearray= lineinfoS.split(b'\n')
-#     for individL in linearray:
-#         print(individL)
-#         print("____")
+# print(r.stdout)
+
+test_data = b"author hallewhittaker\nauthor-mail <88335095+hallewhittaker@users.noreply.github.com>\nauthor-time 1627917128\nauthor-tz +0100\ncommitter GitHub\ncommitter-mail <noreply@github.com>\ncommitter-time 1627917128\ncommitter-tz +0100\nsummary Create README.md\nboundary\nfilename README.md\n\t--->\n'"
+tab = test_data.split(b'\t') 
+
+
+arrayofDictionaries = []
+
+
+for lineinfoS in tab:
+    print(lineinfoS)
+    print("----")
+    linearray= lineinfoS.split(b'\n')
+    tempdictionary = {}
+    for individL in linearray:
+        print(individL)
+        tempkeyN = individL.split(b" ")[0]
+        print(tempkeyN)
+
+        print("____")
+        tempdictionary[tempkeyN] = individL
+        # breaks
+    arrayofDictionaries.append(tempdictionary)
+    # break
+
+# this just prints things out nicely
+# import json
+print(arrayofDictionaries)
+
 #currently splits tabs and new lines and prints this edited format.
 
 # cheader =[]
@@ -33,7 +54,7 @@ for currentDictionary in arrayofD:
 # b="b'boundary'" #work
 # c="b'author-mail <88335095+hallewhittaker@users.noreply.github.com>'" #work
 
-# pattern = r"\s+|[']|'(?=\s)"
+# #pattern = r"\s+|[']|'(?=\s)"
 # result = [s for s in re.split(pattern, c) if s]
 # print(result)
 
