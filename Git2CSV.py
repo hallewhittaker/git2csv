aaa7f38c80db56304aa6693ad46f897b3c6ef64b
Halle Whittaker <whittaker.halle@gmail.com>
2021-08-24 20:52:30 +0100

    Cleaned Up Code 24/08/2021

--- Git2CSV.py
+++ Git2CSV.py
@@ -42,11 +42,11 @@ if overwrite_boolean == False:
         None
 
 if filepath != None:
-    mydir_tmp = filepath #"C:\Users\Whitt\hallewhittaker\FormatFuzzer"
+    mydir_tmp = filepath
     mydir_new = os.chdir(mydir_tmp) 
     mydir = os.getcwd() 
 else:
-    mydir_tmp = mydir_static_copy #"C:\\Users\\Whitt\\hallewhittaker" 
+    mydir_tmp = mydir_static_copy 
     mydir_new = os.chdir(mydir_tmp) 
     mydir = os.getcwd()
 
@@ -140,7 +140,7 @@ for i in range(len(finalArray)):
                 arrayofDictionaries.append(tempdictionary)
                 tempdictionary = {} 
                 count = 0
-
+                
 # if debugging_level != None:
 #     logging.debug("Final Array:" + str(arrayofDictionaries))
 
@@ -158,10 +158,9 @@ try:
 except:
     None
 
-#CSV
 filename_csvE = output_file
 isExist1 = os.path.exists(mydir_static_copy + '\\{}'.format(filename_csvE))
-if filepath != None and overwrite_boolean ==True and specified_format =="csv" and isExist1 ==True: #dealing with overwrites   
+if filepath != None and overwrite_boolean ==True and specified_format =="csv" and isExist1 ==True:  
     filename_csv = "TestCSV.csv"
     isExist = os.path.exists(mydir_static_copy + '\\{}'.format(filename_csv))
     if isExist == True:
@@ -170,129 +169,79 @@ if filepath != None and overwrite_boolean ==True and specified_format =="csv" an
             writer.writeheader()
             writer.writerows(rows)
 
-if filepath != None and specified_format == None and filetype != "csv" and filetype != "json": #no format, no filetype csv=default
+if filepath != None and specified_format == None and filetype != "csv" and filetype != "json": 
     filename_csv = output_file
     with open(mydir_static_copy + '\\{}'.format(filename_csv), "w", encoding='ISO-8859-1', newline='') as sys.stdout:
             writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
             writer.writeheader()
             writer.writerows(rows)
 
-if filepath != None and specified_format == "csv" and output_file == '-': #stdout
+if filepath != None and specified_format == "csv" and output_file == '-': 
     writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames, lineterminator='\n')
     writer.writeheader()
     writer.writerows(rows)
 
-if filepath != None and specified_format == "csv" and isExist1 == False and output_file != '-': # format is csv and output file doesnt exist 
+if filepath != None and specified_format == "csv" and isExist1 == False and output_file != '-': 
     with open(mydir_static_copy + '\\{}'.format(filename_csvE), "w", encoding='ISO-8859-1', newline='') as sys.stdout:
             writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
             writer.writeheader()
             writer.writerows(rows)
 
-elif filepath != None and specified_format == "csv" and list_of_files == None: #format first priority
+elif filepath != None and specified_format == "csv" and list_of_files == None: 
     filename_csv= "TestCSV.csv"
     with open(mydir_static_copy + '\\{}'.format(filename_csv), "w", encoding='ISO-8859-1', newline='') as sys.stdout:
             writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
             writer.writeheader()
             writer.writerows(rows)
 
-elif specified_format == "json": #if format json, nothing
+elif specified_format == "json": 
     None
 
-elif filepath != None and filetype == "csv": #filetype second priority 
+elif filepath != None and filetype == "csv": 
     filename_csv = output_file
     with open(mydir_static_copy + '\\{}'.format(filename_csv), "w", encoding='ISO-8859-1', newline='') as sys.stdout:
             writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
             writer.writeheader()
             writer.writerows(rows)
 
-if filepath == None: #no arguments
+if filepath == None: 
     filename_csv = 'TestCSV.csv'
     with open(mydir_static_copy + '\\{}'.format(filename_csv), 'w', encoding='ISO-8859-1', newline='') as f: 
         writer = csv.DictWriter(f, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(rows)
 
-#JSON
 filename_jsonE = output_file
 isExist2 = os.path.exists(mydir_static_copy + '\\{}'.format(filename_jsonE))
-if filepath != None and overwrite_boolean ==True and specified_format =="json" and  isExist2==True: #dealing with overwriting
+if filepath != None and overwrite_boolean ==True and specified_format =="json" and  isExist2==True: 
     filename_json = "data.json"
     isExist = os.path.exists(mydir_static_copy + '\\{}'.format(filename_json))
     if isExist == True:
         with open(mydir_static_copy + '\\{}'.format(filename_json), "w+", encoding='ISO-8859-1', newline='')  as sys.stdout:
             sys.stdout.write(jsonstring)
 
-if filepath != None and specified_format == "json" and output_file == '-': #stdout 
+if filepath != None and specified_format == "json" and output_file == '-':  
     sys.stdout.write(jsonstring)
     sys.stdout.close()
 
-if filepath != None and specified_format == "json" and isExist2 == False and output_file != '-': # format json and file doesnt exist, json output to new file
+if filepath != None and specified_format == "json" and isExist2 == False and output_file != '-': 
     with open(mydir_static_copy + '\\{}'.format(filename_jsonE), "w", encoding='ISO-8859-1', newline='')  as sys.stdout:
             sys.stdout.write(jsonstring)
 
-elif filepath != None and specified_format == "json" and list_of_files == None: #specifying format first
+elif filepath != None and specified_format == "json" and list_of_files == None: 
     filename_json= "data.json"
     with open(mydir_static_copy + '\\{}'.format(filename_json), "w", encoding='ISO-8859-1', newline='')  as sys.stdout:
             sys.stdout.write(jsonstring) 
 
-elif specified_format == "csv": #if csv do nothing.
+elif specified_format == "csv": 
     None
 
-elif filepath != None and  filetype =="json": #specifying filetype second priority
+elif filepath != None and  filetype =="json": 
     filename_json= output_file
     with open(mydir_static_copy + '\\{}'.format(filename_json), "w", encoding='ISO-8859-1', newline='')  as sys.stdout:
             sys.stdout.write(jsonstring) 
 
-if filepath == None: # no arguments entered
+if filepath == None: 
     filename_json= "data.json"
     with open(mydir_static_copy + '\\{}'.format(filename_json), "w", encoding='ISO-8859-1', newline='')  as jsonFile:   
         jsonFile.write(jsonstring)
-
-
-
-
-
-
-#JSON
-#py Git2CSV.py --format json C:\Users\Whitt\hallewhittaker\FormatFuzzer TestCSV.txt (Working: Expected JSON output to new file called TestCSV.txt)
-#py Git2CSV.py C:\Users\Whitt\hallewhittaker\FormatFuzzer data.json (Working: Expected Exception Raised)
-#py Git2CSV.py --format json C:\Users\Whitt\hallewhittaker\FormatFuzzer TestCSV.csv (Working: Expected Exception Raised)
-
-#CSV 
-#py Git2CSV.py --format csv C:\Users\Whitt\hallewhittaker\FormatFuzzer data.json (Working: Expected Exception Raised)
-#py Git2CSV.py --format csv C:\Users\Whitt\hallewhittaker\FormatFuzzer TestCSV.txt (Working: Expected CSV output to new file called TestCSV.txt)
-#py Git2CSV.py C:\Users\Whitt\hallewhittaker\FormatFuzzer TestCSV.csv (Working: Expected Exception Raised)
-
-#Stdout
-#py Git2CSV.py --format json C:\Users\Whitt\hallewhittaker\FormatFuzzer - (Working: STDOUT in JSON output)
-#py Git2CSV.py --format csv C:\Users\Whitt\hallewhittaker\FormatFuzzer - (Working: STDOUT in CSV output)
-#py Git2CSV.py --format csv --filelist ListofFiles.txt C:\Users\Whitt\hallewhittaker\FormatFuzzer - > TestCSV.csv (Working: Reads from listoffiles.txt then prints data to csv file)
-#py Git2CSV.py --format json --filelist ListofFiles.txt C:\Users\Whitt\hallewhittaker\FormatFuzzer - > data.json (Working: Reads from listoffiles.txt then prints data to json file)
-
-#JSON Overwriting
-#py Git2CSV.py --format json --overwrite_existing C:\Users\Whitt\hallewhittaker\FormatFuzzer data.json  (Working: Json output to data.json)
-#py Git2CSV.py --format json --overwrite_existing C:\Users\Whitt\hallewhittaker\FormatFuzzer TestCSV.csv (Working: Json output to format json)
-#py Git2CSV.py --format json --overwrite_existing C:\Users\Whitt\hallewhittaker\FormatFuzzer TestCSV.txt (Working: Json output to new file TestCSV.txt)
-#py Git2CSV.py --overwrite_existing C:\Users\Whitt\hallewhittaker\FormatFuzzer data.json (Working: Json output to data.json) 
-#py Git2CSV.py --format json C:\Users\Whitt\hallewhittaker\FormatFuzzer TestCSV.txt (Working:Json output in TestCSV.txt file)
-
-#CSV Overwriting 
-#py Git2CSV.py --format csv --overwrite_existing C:\Users\Whitt\hallewhittaker\FormatFuzzer data.json (Working: CSV output to testcsv.csv)
-#py Git2CSV.py --format csv --overwrite_existing C:\Users\Whitt\hallewhittaker\FormatFuzzer TestCSV.csv (Working: CSV output to testcsv.csv)
-#py Git2CSV.py --format csv --overwrite_existing C:\Users\Whitt\hallewhittaker\FormatFuzzer TestCSV.txt (Working: CSV output to new file)
-#py Git2CSV.py --overwrite_existing C:\Users\Whitt\hallewhittaker\FormatFuzzer TestCSV.csv (Working: CSV output to testcsv.csv) 
-#py Git2CSV.py C:\Users\Whitt\hallewhittaker\FormatFuzzer data.txt (Working: No format specified, csv output in new output file )
-
-#JSON Debugging
-#py Git2CSV.py --format json --overwrite_existing --debugging_level DEBUG C:\Users\Whitt\hallewhittaker\FormatFuzzer data.json (Working: Debugs as expected and enters data to data.json)
-#py Git2CSV.py --format json --overwrite_existing C:\Users\Whitt\hallewhittaker\FormatFuzzer data.json (Working: Json output to data.json)
-#py Git2CSV.py --format json --debugging_level DEBUG C:\Users\Whitt\hallewhittaker\FormatFuzzer data.json (Working: Raised Exception)
-#py Git2CSV.py --format json --debugging_level ERROR C:\Users\Whitt\hallewhittaker\FormatFuzzer data.json (Working: Raised Exception)
-
-#CSV Debugging
-#py Git2CSV.py --format csv --overwrite_existing --debugging_level DEBUG C:\Users\Whitt\hallewhittaker\FormatFuzzer TestCSV.csv (Working: Debugs as expected and enters output to TestCSV.csv)
-#py Git2CSV.py --format csv --overwrite_existing C:\Users\Whitt\hallewhittaker\FormatFuzzer TestCSV.csv (Working: CSV output to TestCSV,csv)
-#py Git2CSV.py --format csv --debugging_level DEBUG C:\Users\Whitt\hallewhittaker\FormatFuzzer TestCSV.csv (Working: Raised Exception)
-#py Git2CSV.py --format csv --debugging_level ERROR C:\Users\Whitt\hallewhittaker\FormatFuzzer TestCSV.csv (Working: Raised Exception)
-
-
